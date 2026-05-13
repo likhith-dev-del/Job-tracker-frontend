b7d74701007d11f36bf99c7f8f52008b5513b8da
Likith <sappalikhith5@gmail.com>
2026-05-13 17:56:46 +0530

    Fixed register API

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -76,7 +76,7 @@ export default function RegisterPage() {
     
  try {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/auth/register`,
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/register`,
     {
       method: "POST",
       headers: {
